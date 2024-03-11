0d113696f81f0e2c3a0ee1bd76189a20f6fc61e8
woollim <begin2125@gmail.com>
2024-03-11 22:36:57 +0900

    1. 첫번째 업로드

---
+++ ArenaBattle/Source/ArenaBattle/ArenaBattle.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ArenaBattle.h"
+#include "Modules/ModuleManager.h"
+
+DEFINE_LOG_CATEGORY(ArenaBattle);
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ArenaBattle, "ArenaBattle" );
