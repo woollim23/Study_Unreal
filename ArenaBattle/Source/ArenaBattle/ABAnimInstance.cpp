0d96d9704f80b3e424b4590aa5de6b5864fca99c
woollim <begin2125@gmail.com>
2024-03-25 23:34:09 +0900

    16. 블루프린트 불 변수 구현

---
+++ ArenaBattle/Source/ArenaBattle/ABAnimInstance.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ABAnimInstance.h"
+
+UABAnimInstance::UABAnimInstance()
+{
+	CurrentPawnSpeed = 0.0f;
+}
---
+++ ArenaBattle/Source/ArenaBattle/ABAnimInstance.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "ArenaBattle.h"
+#include "Animation/AnimInstance.h"
+#include "ABAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARENABATTLE_API UABAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	UABAnimInstance();
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Pawn, Meta = (AllowPrivateAccess = true))
+	float CurrentPawnSpeed;
+};
